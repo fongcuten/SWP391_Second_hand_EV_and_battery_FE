5a717d710050e8f0fb623912e89435c17ba467b9
KngoVM <kietisrise@gmail.com>
2025-11-15 10:58:09 +0700

    remove unused import

--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { LogOut } from "lucide-react";
 import { useAuth } from "../../contexts/AuthContext";
 import {
